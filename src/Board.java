3cb061b6bbb067e6ee8396b5efb3e4655456afbf
Marcin Duplaga <duplaga.marcin.com>
2019-03-31 00:27:05 +0100

    wstępna modyfikacja wyswietalnia planszy

--- src/Board.java
+++ src/Board.java
@@ -7,7 +7,10 @@ public class Board { // utworzenie planszy do gry, z polami
 
     private ArrayList pieces = new ArrayList<Piece>();
 
-    public Board(int color) {    // utworzenie planszy szachowej
+    private final int white = 1;
+    private final int black = 1;
+
+    public Board() {    // utworzenie planszy szachowej, ustawienie początkowe
 
         for(int column = 0; column < squares.length; column++){
             for(int row = 7; row >= 0; row--){
@@ -15,21 +18,30 @@ public class Board { // utworzenie planszy do gry, z polami
             }
         }
 
-        if(color == 1){ // wybór gry białymi figurami
-            for(int column = 0; column < squares.length; column++){  // inicjalizacja pionków na planszy
-                pieces.add(new Pawn(column,6, color));
-                squares[column][6] = new Square("P"); // tworzenie pionków
-            }
-            squares[0][7] = new Square("R"); // tworzenie wież
-            squares[7][7] = new Square("R");
-            squares[1][7] = new Square("N");
-            squares[6][7] = new Square("N");
-            squares[2][7] = new Square("B");
-            squares[5][7] = new Square("B");
-            squares[4][7] = new Square("Q");
-            squares[3][7] = new Square("K");
-
+        for(int column = 0; column < squares.length; column++){  // inicjalizacja pionków na planszy
+            pieces.add(new Pawn(column,6, white));
+            pieces.add(new Pawn(column, 1, black));
+            squares[column][6] = new Square("P"); // tworzenie pionków białych i czarych
+            squares[column][1] = new Square("P");
         }
+        squares[0][7] = new Square("R"); // tworzenie wież oraz figur białych
+        squares[7][7] = new Square("R");
+        squares[1][7] = new Square("N");
+        squares[6][7] = new Square("N");
+        squares[2][7] = new Square("B");
+        squares[5][7] = new Square("B");
+        squares[4][7] = new Square("Q");
+        squares[3][7] = new Square("K");
+
+        squares[0][0] = new Square("R"); // tworzenie wież oraz figur czarnych
+        squares[7][0] = new Square("R");
+        squares[1][0] = new Square("N");
+        squares[6][0] = new Square("N");
+        squares[2][0] = new Square("B");
+        squares[5][0] = new Square("B");
+        squares[3][0] = new Square("Q");
+        squares[4][0] = new Square("K");
+
 
     }
     public Square getSquare(int column, int row){   // zapewnia dostep do danego pola
--- src/Game.java
+++ src/Game.java
@@ -4,11 +4,11 @@ public class Game {
     // chwilowo tymczasowy main, wrzucam sobie różne rzeczy, aby sprawdzić czy funkcjonalności działają :)
     public static void main(String[] args) {
 
-        Scanner which_color = new Scanner(System.in);
+       /* Scanner which_color = new Scanner(System.in);
         System.out.println("White(1) or Black(2)");
-        int color = Integer.parseInt(which_color.nextLine());
+        int color = Integer.parseInt(which_color.nextLine());*/
 
-        Board board = new Board(color);
+        Board board = new Board();
 
         for (int row = 0; row < 8; row++) { // sprawdzenie poprawności wyświetlania szachownicy
             for (int column = 0; column < 8; column++) {
