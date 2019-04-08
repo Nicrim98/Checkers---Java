0fa513baf5a85f948add4f67062d9ceea36c70b4
Marcin Duplaga <duplaga.marcin.com>
2019-04-08 16:36:35 +0200

    Board display fixed

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,10 @@ import java.util.List;
 
 public class Board { // utworzenie planszy do gry, z polami
 
-    private Square[][] squares = new Square[8][8];
+    private static final int width = 8;
+    private static final int height = 8;
+
+    private Square[][] squares = new Square[width][height];
 
     private ArrayList pieces = new ArrayList<Piece>(); // Jak na razie jedyna arraylista
 
@@ -12,15 +15,16 @@ public class Board { // utworzenie planszy do gry, z polami
 
     public Board() {    // utworzenie planszy szachowej, ustawienie początkowe
 
-        for(int column = 0; column < squares.length; column++){
-            for(int row = 7; row >= 0; row--){
+        for (int column = 0; column < width; column++) {
+            for (int row = 0; row < height; row++) {
                 this.squares[column][row] = new Square(column, row);
             }
         }
+
         // nadpisanie ustawienie początkowego pionkami do gry zamiast pustych pól, które są tworzone powyżej
 
         for(int row = 0; row < 3; row++){  // białe pionki - początkowe ustawienie
-            for(int column = 1; column < 8; column++) {
+            for(int column = 1; column < 8;column += 2){
                 if (row % 2 == 0) {
                     this.squares[column][row] = new Square("W");
                     // dodać do array listy pieces - pionki
@@ -31,8 +35,8 @@ public class Board { // utworzenie planszy do gry, z polami
             }
         }
 
-        for(int row = 5; row < 8; row++){  // białe pionki - początkowe ustawienie
-            for(int column = 1; column < 8; column++) {
+        for(int row = 5; row < height; row++){  // białe pionki - początkowe ustawienie
+            for(int column = 1; column < width; column += 2) {
                 if (row % 2 == 1) {
                     this.squares[column-1][row] = new Square("B");
                     // dodać do array listy pieces - pionki
@@ -49,3 +53,4 @@ public class Board { // utworzenie planszy do gry, z polami
     }
 
 }
+
